f108e83c3b5a567bbf3933d96669ae36d093664f
snu404 <53073565+snu404@users.noreply.github.com>
2026-04-03 09:48:40 +0900

    Add Firebase configuration file

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+// 본인 Firebase 콘솔 값 입력
+export const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT.firebaseapp.com",
+  projectId: "YOUR_PROJECT",
+  storageBucket: "YOUR_PROJECT.appspot.com",
+  messagingSenderId: "XXXX",
+  appId: "XXXX"
+};
