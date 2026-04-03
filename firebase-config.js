51c08d102f51cd4007bb28a99375a7cefff79dcb
snu404 <53073565+snu404@users.noreply.github.com>
2026-04-03 10:29:37 +0900

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,13 @@
 // 본인 Firebase 콘솔 값 입력
 export const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "XXXX",
-  appId: "XXXX"
+  apiKey: "AIzaSyD-3V3ZZwh05qLkYN5ubWaXKkgQa-2psMc",
+  authDomain: "jck2026-989bb.firebaseapp.com",
+  projectId: "jck2026-989bb",
+  storageBucket: "jck2026-989bb.firebasestorage.app",
+  messagingSenderId: "41489628927",
+  appId: "1:41489628927:web:4feee05088456d9bf016ca",
 };
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+//  measurementId: "G-63KPECK6NN"
