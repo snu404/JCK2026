4cb1609ff6f1e03a789a5ef750dd644291699de0
snu404 <53073565+snu404@users.noreply.github.com>
2026-05-16 09:49:38 +0900

    Update app.js

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ import {
   getAuth,
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
+  sendPasswordResetEmail,
   onAuthStateChanged,
   signOut
 } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
